e04bdcc40daf29246ece1aa19f68567c153d4eec
Vail Gold <vail130@gmail.com>
2016-02-18 16:11:16 +0800

    Made computer player move randomly.

--- tenzen/computer_player.py
+++ tenzen/computer_player.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 
+import random
+
 from tenzen.exceptions import PassTurn
 
 
@@ -13,15 +15,19 @@ class ComputerPlayer(object):
         if self.board.is_complete():
             raise PassTurn()
 
-        # TODO: Magic
+        possible_moves = set()
         for y in range(self.board.dimension):
             for x in range(self.board.dimension):
                 if (x, y) not in self.invalid_moves:
-                    try:
-                        self.board.add_piece(coordinates=[x, y], color=self.color)
-                    except ValueError:
-                        pass
-                    else:
-                        return x, y
+                    possible_moves.add((x, y))
+
+        while possible_moves:
+            x, y = random.choice(list(possible_moves))
+            try:
+                self.board.add_piece(coordinates=[x, y], color=self.color)
+            except ValueError:
+                possible_moves.remove((x, y))
+            else:
+                return x, y
 
         raise PassTurn()
